cdc106f0d844df336110f15d9233b464900e4071
apsschotten <161073410+apsschotten@users.noreply.github.com>
2024-11-25 17:15:49 -0400

    Update Registro.jsx

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,9 +1,31 @@
 import { useState } from "react";
 
 export default function Registrar() {
+  event.preventDefault();
+  try {
+    await fetch('http://localhost:3000/usuarios', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        nome: nome,
+        email: email
+      })
+    });
+  } catch {
+    alert("Ocorreu um erro.");
+  }
 
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
   return (
-        
+    <main>
+      <form onSubmit={Registrar}>
+        <imput value={nome}>Nome</imput>
+        <imput value={email}>E-mail</imput>
+        <button>Enviar</button>
+      </form>
+    </main>
   );
 }
