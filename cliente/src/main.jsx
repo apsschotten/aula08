3ee94120f189d0c6af95e10320fd4fd2b8e76ba5
ana <ana.schotten@estudante.ifms.edu.br>
2024-12-02 16:50:59 -0400

    .

--- cliente/src/main.jsx
+++ cliente/src/main.jsx
@@ -2,13 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import RegistrarPessoa from './pages/Registro'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-       <Routes>
-            <Route path="/" element={<Home/>}/>
-       </Routes>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/registrar" element={<RegistrarPessoa />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,27 +1,35 @@
 import { useState } from "react";
+import { useNavigation } from 'react-router-dom';
 
-const Registrar = async (event) => {
-  event.preventDefault();
-  try {
-    await fetch('http://localhost:3000/usuarios', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        nome: nome,
-        email: email
-      })
-    });
-  } catch {
-    alert("Ocorreu um erro.");
+export default function RegistrarPessoa() {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+
+  const navigate = useNavigation();
+
+  const registrar = async (event) => {
+    event.preventDefault();
+    try {
+      const resposta = await fetch('http://localhost:3000/usuarios', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          nome: nome,
+          email: email
+        })
+      });
+    } catch {
+      alert("Ocorreu um erro.");
+    }
+
+    return (
+      <main>
+        <form onSubmit={registrar}>
+          <input type="text" id="" value={nome} onChange={(event) => setNome(event.target.value)}>Nome</input>
+          <input type="email" id="" value={email} onChange={(event) => setEmail(event.target.value)}>E-mail</input>
+          <button>Enviar</button>
+        </form>
+      </main>
+    );
   }
-
-  return (
-    <main>
-      <form onSubmit={Registrar}>
-        <input type="text" id="" value={nome} onChange={(event) => setNome(event.target.value)}>Nome</input>
-        <input type="email" id="" value={email} onChange={(event) => setEmail(event.target.value)}>E-mail</input>
-        <button>Enviar</button>
-      </form>
-    </main>
-  );
 }
